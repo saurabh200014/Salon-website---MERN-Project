50dc4f4e91c47d157220ed67d554e290bddaa095
rajen <2019saurabh.borawake@ves.ac.in>
2022-05-08 11:47:36 +0530

    server created

---
+++ app.js
@@ -0,0 +1,8 @@
+const express=require('express');
+const app=express();
+app.get('/',(req,res)=>{
+    res.send('Hello from server!');
+})
+app.listen(3000,()=>{
+    console.log('server is running at 3000');
+})
